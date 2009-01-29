82e3b1fae086e4917e40dc41b36639ab9ae78399
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-29 23:57:29 +0000

    - some proper tests with test data

--- t/01_objects.rb
+++ t/01_objects.rb
@@ -1,5 +1,53 @@
+require 'test/unit'
 require 'digest_shingles'
 
+$test_text = <<TEST_TEXT
+this is some simple text for testing the shingles functionality
+consisting of a few words without any punctuation merely because
+said punctuation would be removed without regard for its safety
+TEST_TEXT
+
+$jumbled = ($test_text.split(' ').sort_by{|x|x.length}).join(' ')
+
+p $jumbled
+
+class Test_Shingles < Test::Unit::TestCase
+    def test_f1_f2
+        shinglers = Digest::Shingle::Archive.new()
+        shinglers.add_file('t/f1')
+        shinglers.add_file('t/f2')
+        f1 = shinglers.match_file('t/f1')
+        f2 = shinglers.match_file('t/f2')
+        f3 = shinglers.match_file('t/f3')
+        assert(f1, 'matches f1')
+        assert(f2, 'matches f2')
+        assert_equal(false, f3, 'no match for f3')
+    end
+
+    def test_simplistic
+        a = Digest::Shingle::Archive.new()
+
+        s1 = a.add($test_text, 's1')
+        s2 = a.add($test_text, 's2')
+        assert_equal(true, a.match(s2), 's2 matches s1')
+        assert_equal(true, a.match(s1), 's1 matches s2')
+
+        s3 = Digest::Shingle.new('nothing goes here')
+        assert_equal(false, a.match(s3), 's3 does not match')
+    end
+
+    def test_jumbled
+        a = Digest::Shingle::Archive.new()
+
+        s1 = a.add($test_text, 's1')
+        s2 = Digest::Shingle.new($jumbled)
+
+        assert_equal(false, a.match(s2), 'jumbled matches nothing')
+    end
+
+end
+
+__END__
 store, test = [], []
 seen = nil
 ARGV.each { |i|
