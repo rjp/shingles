c13820c0ad7c265e7467c72d8e29e0092976b127
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-30 08:50:12 +0000

    - more tests - TODO make matches(text) work to correct failing test

--- t/01_objects.rb
+++ t/01_objects.rb
@@ -7,21 +7,64 @@ consisting of a few words without any punctuation merely because
 said punctuation would be removed without regard for its safety
 TEST_TEXT
 
+# this text shouldn't match $test_text because the word order is different
 $jumbled = ($test_text.split(' ').sort_by{|x|x.length}).join(' ')
 
-p $jumbled
-
 class Test_Shingles < Test::Unit::TestCase
-    def test_f1_f2
+    def test_f1_f2_f3
         shinglers = Digest::Shingle::Archive.new()
         shinglers.add_file('t/f1')
         shinglers.add_file('t/f2')
         f1 = shinglers.match_file('t/f1')
         f2 = shinglers.match_file('t/f2')
         f3 = shinglers.match_file('t/f3')
-        assert(f1, 'matches f1')
-        assert(f2, 'matches f2')
+        assert(f1, 'matches directly added f1')
+        assert(f2, 'matches directly added f2')
         assert_equal(false, f3, 'no match for f3')
+    end
+
+    def test_text_lookup
+        a = Digest::Shingle::Archive.new()
+        # TODO find these files automatically
+        a.add_file('t/f1')
+        a.add_file('t/f2')
+        a.add_file('t/f3')
+        a.add_file('t/f4')
+
+        t = File.new('t/f5').readlines.join("\n").downcase
+        r = a.matches(t, 0.75)
+        p r
+    end
+
+
+    def test_files
+        a = Digest::Shingle::Archive.new()
+        # TODO find these files automatically
+        a.add_file('t/f1')
+        a.add_file('t/f2')
+        a.add_file('t/f3')
+        a.add_file('t/f4')
+
+        t = File.new('t/f5').readlines.join("\n").downcase
+        s = Digest::Shingle.new(t)
+        r = a.matches(s, 0.75)
+        p r
+    end
+
+    def test_f1_direct_f2
+        a = Digest::Shingle::Archive.new()
+
+        a.add_file('t/f1')
+        f2 = a.match_file('t/f2')
+        assert_equal(true, f2, 'f2 matches archived f1')
+    end
+
+    def test_f3_direct_f4
+        a = Digest::Shingle::Archive.new()
+
+        a.add_file('t/f3')
+        r = a.match_file('t/f4')
+        assert_equal(false, r, 'f4 no match for archived f3')
     end
 
     def test_simplistic
