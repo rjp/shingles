32d8085481d6f8aad27eb79f8985b9bc0f325b42
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-02-13 19:42:45 +0000

    - remove dead code

--- t/01_objects.rb
+++ t/01_objects.rb
@@ -89,32 +89,3 @@ class Test_Shingles < Test::Unit::TestCase
     end
 
 end
-
-__END__
-store, test = [], []
-seen = nil
-ARGV.each { |i|
-    if i == '--' then seen = 1; next; end
-    (seen.nil? ? store : test).push i
-}
-p store
-p test
-
-r = []
-shinglers = Digest::Shingle::Archive.new()
-store.each { |file|
-    puts "sketching #{file}"
-    text = File.new(file).readlines.join("\n").downcase
-    s = shinglers.add(text, file)
-    r.push s
-}
-
-test.each { |file|
-    puts "testing #{file}"
-    text = File.new(file).readlines.join("\n").downcase
-    s = Digest::Shingle.new(text)
-    p shinglers.match(s, 0.90)
-	r.each { |q|
-		q.match(s)
-	}
-}
